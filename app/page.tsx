b03213368fb9df62b25a618e92613f414f89b515
KISHOREkevin <kishorekevin3289@gmail.com>
2026-01-31 19:53:46 +0530

    space cut

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@ import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 import { events } from "@/lib/constants"
 
-
-
-
-
 const page = () => {
   return (
     <section>
